8bcd7ce66e3f8ebf949b1a74bf1b86844ed3782a
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-20 16:30:01 -0400

    Update react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+
+function App(){
+    return(
+        //BEM
+        <div className='app'>
+            <h1>Let's build this amazon store!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD__POrtDEBnttA0UbFOdx6qs-7HgP-19w",
+    authDomain: "clone-58ad4.firebaseapp.com",
+    projectId: "clone-58ad4",
+    storageBucket: "clone-58ad4.appspot.com",
+    messagingSenderId: "221982758363",
+    appId: "1:221982758363:web:08d4f6f7f70b86b61783fe",
+    measurementId: "G-PYK59QTH48"
+  };
