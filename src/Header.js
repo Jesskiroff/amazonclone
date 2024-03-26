17d0d1dd40ae55ed5af1cf1872670f9c6bd4fde3
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-26 14:55:14 -0400

    add basket icon

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
-
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
@@ -13,34 +13,31 @@ function Header() {
       />
 
       <div className="header__search">
-        <input 
-        className="header__searchInput" 
-        type="text" />
-        <SearchIcon 
-        className="header__searchIcon"/>
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <div className='header__option'>
-          <span
-          className='header__optionLineOne'>Hello Guest</span>
-          <span
-          className='header__optionLineTwo'>Sign In</span>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
 
-        <div className='header__option'>
-          <span
-            className='header__optionLineOne'>Returns</span>
-            <span
-            className='header__optionLineTwo'>& Orders</span>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
 
-        <div className='header__option'>
-          <span
-              className='header__optionLineOne'>Your</span>
-              <span
-              className='header__optionLineTwo'>Prime</span>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
 
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span
+            className="header__optionLineTwo
+            header__basketCount">0</span>
+        </div>
       </div>
     </div>
   );
