80c456feb9f163371631095b3293fa0fba666f27
Tova Sauer <sauertova1@gmail.com>
2024-03-27 16:43:09 -0400

    connected email and password

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css'
 import {Link} from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault()
+    }
+
+    const register = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className='login'>
             <Link to= '/'>
@@ -17,19 +28,21 @@ function Login() {
 
                 <form>
                     <h5>Email</h5>
-                    <input type='text' />
+                    <input type='text' value={email} onChange= {e => setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type='password' />
+                    <input type='password' value={password} onChange= {e => setPassword(e.target.value)}/>
 
-                    <button className='login_signInButton'>Sign In</button>
+                    <button type='submit' onClick={signIn}
+                    className='login_signInButton'>Sign In</button>
                 </form>
 
                 <p>
                     By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Intrest-Based Ads Notice.
                 </p>
 
-                <button className='login_registerButton'>Create your Amazon Account</button>
+                <button  onClick={register}
+                className='login_registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
